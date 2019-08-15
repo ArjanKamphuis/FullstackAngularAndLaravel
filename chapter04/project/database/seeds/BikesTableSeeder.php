fe34476ce1d4393dcf319abdc7777714df0c1a40
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2019-08-15 15:55:31 +0200

    Chapter 04

---
+++ chapter04/project/database/seeds/BikesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Bike;
+use Illuminate\Database\Seeder;
+
+class BikesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('bikes')->delete();
+        $json = File::get('database/data-sample/bikes.json');
+        $data = json_decode($json);
+        foreach ($data as $obj) {
+            Bike::create([
+                'id' => $obj->id,
+                'make' => $obj->make,
+                'model' => $obj->model,
+                'year' => $obj->year,
+                'mods' => $obj->mods,
+                'picture' => $obj->picture
+            ]);
+        }
+    }
+}
