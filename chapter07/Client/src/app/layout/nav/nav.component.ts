8100f5e886b35c4ff31e0391e08d644cb7065d45
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2019-08-18 00:07:33 +0200

    Chapter 08

--- chapter07/Client/src/app/layout/nav/nav.component.ts
+++ chapter07/Client/src/app/layout/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-nav',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleTagServe: Title) { }
+  
+  setTitle(pageTitle: string) {
+    this.titleTagServe.setTitle(pageTitle);
+  }
 
   ngOnInit() {
   }
